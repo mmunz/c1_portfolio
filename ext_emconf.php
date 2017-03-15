f58ec4f8ee626ce430afbfc1fea46024480650da
Manuel Munz <munz@comuno.net>
2017-03-15 22:42:59 +0100

    raise version to 0.1.0, raise compatibility with core to 8.99.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-7.9.99',
+			'typo3' => '7.6.0-8.9.99',
 		),
 		'conflicts' => array(
 		),
